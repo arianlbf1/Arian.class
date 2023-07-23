ab5df3f94d7038923d2812b3669b25a6ea787014
arianlbf1 <136268373+arianlbf1@users.noreply.github.com>
2023-07-23 15:12:49 -0700

    random idea

---
+++ Russian Roulette.py
@@ -0,0 +1,48 @@
+import random
+import os
+import time
+
+def russian_roulette():
+    done = False
+
+    while not done:
+        pc_number = random.randint(1, 6)
+        print(pc_number)
+        guess = int(input("Enter a number 1 to 6:"))
+
+        print("Checking guess.")
+        time.sleep(5)
+
+        if guess == pc_number:
+            print("Wrong number, you LOST.")
+            os.remove(r"activism.png")
+
+        elif not guess == pc_number:
+            over = input("Good guess, would you like to continue? y/n:")
+            over = over.lower()
+
+            if over == 'y':
+                print("Play again soon, bye.")
+                done = True
+
+            elif over == 'n':
+                print("Feeling lucky, okay.")
+
+            else:
+                print("ERROR")
+
+            
+
+    
+yes_no = input("This is Russian Roulette for you computer. The computer will chose a number out of 6 and if you guess that number, your computer will stop working. Are you sure that you want to play? y/n:")
+yes_no = yes_no.lower()
+
+if yes_no == 'n':
+    print("Okay, bye.")
+
+elif yes_no == 'y':
+    print("Lets start.")
+    russian_roulette()
+
+else:
+    print("ERROR")
