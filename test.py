47e96daaf42e183c9e60ed59424bb1c148a7b3ca
arianlbf1 <136268373+arianlbf1@users.noreply.github.com>
2023-08-16 20:01:27 -0700

    class

--- test.py
+++ test.py
@@ -1,3 +1,45 @@
-import test1
+wordle = "wider"
+temp_wordle = ""
+guess = "berte"
+test = ""
+yellow = ""
 
-print(test1.run())
+loop1 = 5
+loop2 = 6
+loop_n = 5
+
+w_i = 0
+g_i = 0
+w_index = 0
+g_index = 0 
+
+
+temp_wordle = wordle
+while not loop1 == 0:
+    g_i = guess[g_index]
+
+    while not loop2 == 0:
+        if loop2 == 1:
+            yellow += "_"
+            break
+
+        w_i = temp_wordle[w_index]
+
+        if g_i == w_i:
+            temp_wordle = temp_wordle.replace(temp_wordle[w_index], '')
+            print(temp_wordle)
+            yellow += "$"
+            w_index += 1
+            break
+
+        else:
+            w_index += 1
+            
+        loop2 -= 1
+
+    g_index += 1
+    loop2 = len(temp_wordle)
+    loop2 += 1
+    loop1 -= 1
+    w_index = 0
+print(yellow)
--- test1.py
+++ test1.py
@@ -1,17 +1,25 @@
 
 
 wordle = "wider"
+temp_wordle = ""
 guess = "beret"
 temp_wordle = ""
+empty = ""
 
 i = 0
 w_index = ''
 g_index = ''
+w1_i = 0
+g1_i = 0
+w1_index = 0
+g1_index = 0 
+
 green = ""
 yellow = ""
 
 loop = 5
-y_loop = 5
+loop1 = 5
+loop2 = 6
 
 while not loop == 0:
     w_index = wordle[i]
@@ -53,4 +61,34 @@ while not loop == 0:
         pass
 
     elif continue1:
-        pass
+
+        temp_wordle = wordle
+        while not loop1 == 0:
+            g1_i = guess[g1_index]
+
+            while not loop2 == 0:
+                if loop2 == 1:
+                    yellow += "_"
+                    break
+
+                w1_i = temp_wordle[w1_index]
+
+                if g1_i == w1_i:
+                    temp_wordle = temp_wordle.replace(temp_wordle[w1_index], '')
+                    print(temp_wordle)
+                    yellow += "$"
+                    w1_index += 1
+                    break
+
+                else:
+                    w1_index += 1
+                    
+                loop2 -= 1
+
+            g1_index += 1
+            loop2 = len(temp_wordle)
+            loop2 += 1
+            loop1 -= 1
+            w1_index = 0
+    loop -= 0
+print(yellow)
